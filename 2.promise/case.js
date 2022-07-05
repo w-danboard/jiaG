fc57d4119b2cf14e073a27fbb3e403263d986b3d
王琳 <wanglin@wanglindeMacBook-Pro.local>
2022-07-05 15:10:19 +0800

    完善promise

--- 2.promise/case.js
+++ 2.promise/case.js
@@ -126,3 +126,126 @@ let Promise = require('./promise')
 Promise.resolve(123).then(data => {
   console.log(data)
 })
+
+// -------------------------------------------------------
+let Promise = require('./promise')
+
+// Promise.all Promise.race promisify
+let fs = require('fs')
+function read (url) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(url, 'utf8', (err, data) => {
+      if (err) return reject(err)
+      resolve(data)
+    })
+  })
+}
+
+// Promise.all的原理
+Promise.all = function (values) {
+  return new Promise((resolve, reject) => {
+    let arr = []
+    let count = 0
+    function processData (key, value) {
+      arr[key] = value // 将结果和数据对应起来
+      if (++count === values.length) {
+        resolve(arr) // 成功后 把结果抛出来
+      }
+    }
+    for (let i = 0; i < values.length; i++) {
+      let current = values[i]
+      let then = current.then
+      // 是一个promise
+      if (then && typeof then === 'function') { // 是一个promise
+        then.call(current, y => { // 是promise的就让promise执行
+          processData(i, y)
+        }, reject) // 如果其中一个promise出错 就停止执行
+      } else {
+        processData(i, current) // 常量直接返回即可
+      }
+    }
+  })
+}
+
+Promise.all([read('./name.txt'), read('./age.txt'), 1, 2, 3]).then(data => {
+  console.log(data)
+}).catch(err => {
+  console.log('err', err)
+})
+
+// -------------------------------------------------------
+let Promise = require('./promise')
+
+// Promise.all Promise.race promisify
+let fs = require('fs')
+// node中的所有方法 都是错误优先 第二个就是结果 bluebird
+
+function promisify (fn) { // 把方法promise化
+  return function (...args) { // ['name.txt', 'utf8']
+    return new Promise((resolve, reject) => {
+      fn(...args, function (err, data) {
+        if (err) reject(err)
+        resolve(data)
+      })
+    })
+  }
+}
+
+let read = promisify(fs.readFile)
+
+function promisifyAll (obj) {
+  for (let key in obj) { // 遍历整个对象 如果是函数 就把方法重写
+    if (typeof obj[key] === 'function') {}
+    obj[key + 'Async'] = promisify(obj[key]) // 每个方法都promise化
+  }
+}
+
+promisifyAll(fs)
+
+fs.readFileAsync('./name.txt', 'utf8').then(data => {
+  console.log(data)
+})
+
+
+// function read (url) {
+//   return new Promise((resolve, reject) => {
+//     fs.readFile(url, 'utf8', (err, data) => {
+//       if (err) return reject(err)
+//       resolve(data)
+//     })
+//   })
+// }
+
+// function write (url) {
+//   return new Promise((resolve, reject) => {
+//     fs.writeFile(url, 'utf8', (err, data) => {
+//       if (err) return reject(err)
+//       resolve(data)
+//     })
+//   })
+// }
+
+// Promise.race的原理 (race赛跑的意思)
+Promise.race = function (values) {
+  return new Promise((resolve, reject) => {
+    for (let i = 0; i < values.length; i++) {
+      let current = values[i]
+      let then = current.then
+      // 是一个promise
+      if (then && typeof then === 'function') { // 是一个promise
+        then.call(current, y => { // 是promise的就让promise执行
+          resolve(y)
+        }, reject) // 如果其中一个promise出错 就停止执行
+      } else {
+        resolve(current)
+        break
+      }
+    }
+  })
+}
+
+Promise.race([read('./name.txt'), read('./age.txt'), 1, 2, 3]).then(data => {
+  console.log(data)
+}).catch(err => {
+  console.log('err', err)
+})
